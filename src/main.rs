558b471979e1a29464db54c26086c1c4bfc906e5
balask-dev <balask1225@gmail.com>
2023-02-08 10:01:57 +0530

    done

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use serde::{Serialize,Deserialize,Debug};
+use reqwest::header::USER_AGENT;
+use reqwest::Error;
+use std::env;
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct User{
+    userId: u32,
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+#[tokio::main]
+async fn main()-> std::io::Result<(),Error>{
+    let res = format!("https://jsonplaceholder.typicode.com/todos");
+    println!("{}",res);
+    env::set_var("RUST_BACKTRACE", "1");
+    let client = reqwest::Client::new();
+    let cli = client.get(&res).header(USER_AGENT,"rustwebapp").send().await?;
+    let result:Vec<User> = cli.json().await?;
+    println!("{:?}",result);
+    Ok(())
+}
